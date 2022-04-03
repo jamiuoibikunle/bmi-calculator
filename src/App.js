f66ef1512808e3a885b01aa001f1b5789ee289a1
Ibikunle Jamiu <98085958+thealphameil@users.noreply.github.com>
2022-04-03 22:26:22 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,21 +3,25 @@ import './App.css';
 
 function App() {
 
-  const [ HeightValue, setHeightValue ] = useState(null)
   const [ WeightValue, setWeightValue ] = useState(null)
+  const [ BMI, setBMI ] = useState(null)
 
   return (
 
     <div className="App">
       <form>
         <div className='bmi'>
-          BMI: { (WeightValue / Math.pow(HeightValue, 2)).toFixed(2) }
+          Your BMI: {BMI}
         </div>
-        <input type='text' name='value' placeholder='Your weight in kilogrammes.' onChange={(e) => setWeightValue(Number(e.target.value))} />
-        <input type='text' name='value' placeholder='Your height in metres.' onChange={(e) => setHeightValue(Number(e.target.value))} />
+        <input type='text' name='value'
+          placeholder='Your weight in kilogrammes.'
+          onChange={(e) => setWeightValue(Number(e.target.value))} />
+        <input type='text' name='value'
+          placeholder='Your height in metres.'
+          onChange={(e) => setBMI((WeightValue / Math.pow(Number(e.target.value), 2)).toFixed(2))} />
       </form>
     </div>
-  );
+  )
 }
 
 export default App;
